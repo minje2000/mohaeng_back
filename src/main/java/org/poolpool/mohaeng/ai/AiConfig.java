ff0833883f2646342fe86b29fbdf4ebef32f7c1c
minsu <soom990906@gmail.com>
2026-03-10 16:31:45 +0900

    feat) aiconfig 추가

---
+++ src/main/java/org/poolpool/mohaeng/ai/AiConfig.java
@@ -0,0 +1,14 @@
+package org.poolpool.mohaeng.ai;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AiConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
